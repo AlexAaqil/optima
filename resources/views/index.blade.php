8711864380656f797156af1d5ecc275bb51e4b36
alexaaqil <alexaaqil.se@gmail.com>
2025-01-12 07:24:37 +0300

    Home Page: added content and styling for the home page.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,72 @@
-<x-general-layout class="Home">
+<x-general-layout class="Home_Page">
     <section class="Hero">
         <div class="container">
-            <h1>Hero Section</h1>
+            <div class="text">
+                <h1>Empowering Businesses with Tailored Software Solutions</h1>
+                <p>We specialize in developing powerful software to help you streamline your operations, increase productivity and boost revenue.</p>
+
+                <div class="button">
+                    <a href="{{ route('contact') }}" class="btn_link">Get a Free Constulation</a>
+                </div>
+            </div>
         </div>
     </section>
+
+    <section class="Services">
+        <div class="container">
+            <h2>What we do</h2>
+            <div class="services">
+                <div class="service">
+                    <span>01</span>
+                    <span>Custom Software Solutions</span>
+                    <span>We design bespoke software tailored to your unique business requirements, ensuring a seamless fit at cost-effective pricing.</span>
+                </div>
+
+                <div class="service">
+                    <span>02</span>
+                    <span>Software Integration</span>
+                    <span>Enhance your existing systems with modern API integrations, empowering your legacy software with advanced functionality.</span>
+                </div>
+
+                <div class="service">
+                    <span>03</span>
+                    <span>Web Development</span>
+                    <span>Build dynamic, responsive, and visually stunning websites that deliver exceptional user experiences and drive business growth.</span>
+                </div>
+
+                <div class="service">
+                    <span>04</span>
+                    <span>SEO Optimization</span>
+                    <span>Boost your online presence with cutting-edge SEO strategies and digital marketing campaigns designed to increase traffic and conversions.</span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="About_Us">
+        <div class="container">
+            <h2>Why Choose Optima</h2>
+            <div class="abouts">
+                <div class="about">
+                    <span>Proven Expertise</span>
+                    <span>Our team comprises of seasoned professionals with extensive experience across all domains of software engineering.</span>
+                </div>
+    
+                <div class="about">
+                    <span>Innovative Solutions</span>
+                    <span>We leverage cutting-edge technologies to help you outpace competitors and deliver exceptional results.</span>
+                </div>
+    
+                <div class="about">
+                    <span>Effortless Scalability</span>
+                    <span>Our solutions are designed to scale seamlessly, adapting to your evolving needs without limits.</span>
+                </div>
+    
+                <div class="about">
+                    <span>Fully Customizable</span>
+                    <span>We deliver tailor-made solutions uniquely designed to meet your specific business requirements.</span>
+                </div>
+            </div>
+        </div>
+    </section>    
 </x-general-layout>
